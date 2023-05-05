a3e8708fe54fb18d2214667136e7cc20c1fd3c95
cpozo94 <cpg.948@gmail.com>
2023-05-05 17:44:23 +0000

    updating files

--- src/front/js/pages/petgallery.jsx
+++ src/front/js/pages/petgallery.jsx
@@ -14,18 +14,25 @@ export const PetGallery = () => {
   }, []);
 
   return (
-    <div>
-      {pets.map((pet) => (
-        <div key={pet.id} className="card" style={{ width: "300px" }}>
-          <h5 className="card-title">{pet.name}</h5>
-          <div className="card-body">
-            <img src=".." className="card-img-top" />
-            <p className="card-text">
-              {`Name: ${pet.name}, Age: ${pet.age}, Breed: ${pet.breed}`}
-            </p>
+    <div className="container-fluid px-5">
+      <h1 className="titulo">Galería de Mascotas</h1>
+      <div className="row row-cols-1 row-cols-md-4 g-4">
+        {pets.map((pet, index) => (
+          <div key={index} className="col">
+            <div className="card">
+              <img
+                src={pet.photo}
+                className="card-img-top"
+                alt={`Photo of ${pet.name}`}
+              />
+              <div className="card-body">
+                <h5 className="card-title">{pet.name}</h5>
+                <p className="card-text">{`Age: ${pet.age}, Breed: ${pet.breed}`}</p>
+              </div>
+            </div>
           </div>
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
   );
 };
