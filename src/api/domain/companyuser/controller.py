d4f6bbaebd586942aaa1a7652313e9bbad02d90c
cpozo94 <cpg.948@gmail.com>
2023-05-05 11:51:20 +0000

    adding volunteer

--- src/api/domain/companyuser/controller.py
+++ src/api/domain/companyuser/controller.py
@@ -1,23 +1,21 @@
 from flask import jsonify
 import api.domain.company.controller as Company_controller
 import api.domain.user.controller as User_controller
+import api.domain.volunteers.repository as Repository
 
 
 
 
 
-
-
-
-#aquí lo que busco es que llame a los dos  y luego cambio el retorno para que me de el resultado que quiero realmente para que me devuelva el usuario y la compañía
 
 
 def create_user_admin(body):
     user = User_controller.create_user(body["user"], "admin")
     body["company"]["user_id"] = user.id
     company = Company_controller.new_company(body["company"])
-    message = f"User created with id {user.id}"
-    return {"user_id": user.id, "message": message}
+    volunteer = Repository.add_volunteer(user.id, user.id)
+    message = f"Usuario createdo con ID {user.id}, Compañía creada con ID, y voluntario creado con ID:"
+    return {"user_id": user.id , "message": message }
     
 
 #pasarle user.id y company.id para que tb sea voluntario a la vez que admin y tenga sus permisos
