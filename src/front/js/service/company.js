b31502589c0951c2a5079e72ac679eca534740b9
cpozo94 <cpg.948@gmail.com>
2023-05-05 10:20:55 +0000

    front is working but it needs some changes

--- src/front/js/service/company.js
+++ src/front/js/service/company.js
@@ -4,7 +4,7 @@ const HEADERS = {
   "Content-Type": "application/json",
 };
 
-export const createCompany = async (company, userId) => {
+export const createCompany = async (company, userId, onSuccess, onError) => {
   try {
     const response = await fetch(`${URL}/api/register/company/user`, {
       method: "POST",
@@ -13,30 +13,31 @@ export const createCompany = async (company, userId) => {
     });
     const data = await response.json();
     console.log(data);
-    setCompany({
-      name: "",
-      cif: "",
-      logo: "",
-      adress: "",
-      description: "",
-      user_id: "",
-    });
+    onSuccess(); // call the success handler
   } catch (error) {
     console.log("error register company", error);
+    onError(error); // call the error handler with the error object
   }
 };
 
-export const addNewUser = async (data) => {
+export const addNewUserAndCompany = async (
+  userData,
+  companyData,
+  onSuccess,
+  onError
+) => {
   try {
     const response = await fetch(`${URL}/api/register/company/user`, {
       method: "POST",
-      body: JSON.stringify(data),
+      body: JSON.stringify({ user: userData, company: companyData }),
       headers: { "Content-Type": "application/json" },
     });
 
-    const result = await response.json(); // Parse response body as JSON
-    return result.id; // Return ID of new user
+    const data = await response.json(); // Parse response body as JSON
+    console.log(data);
+    onSuccess(data); // call the success handler with the response data
   } catch (err) {
     console.log(err);
+    onError(err); // call the error handler with the error object
   }
 };
