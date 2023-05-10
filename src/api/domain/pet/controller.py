ba0fbdcdcc97a98c15b549d3b0f9cb12b5cfb988
Dihero86 <diegohrodriguez86@gmail.com>
2023-05-10 08:15:55 +0000

    fix: Fwt in route Pet

--- src/api/domain/pet/controller.py
+++ src/api/domain/pet/controller.py
@@ -4,26 +4,34 @@ from api.models.index import Status
 import api.domain.company.controller as Company_contoller
 import json
 
+def get_volunteer_company(user_id,company_id):
+    volunteer = Volunteer_controller.get_volunteer(user_id)
+    if not isinstance(volunteer, CompanyVolunteers): #compruebo que existe el voluntario
+        return {"msg": "Forbidden", "error": True, "status": 403 }
+    company = Company_contoller.get_company(company_id)
+    if not isinstance(company, Company): #compruebo que existe la compañia
+        return {"msg": "Bad Request: Company not Found", "error": True, "status": 404 }
+    if volunteer.company_id != company_id: #compruebo que sean de la misma company
+        return {"msg": "Forbidden", "error": True, "status": 403 }
+    return volunteer
+
 def upload_fotos(fotos):
     array_fotos= list(fotos.lists())
     lista_imagenes = list(map(lambda foto: upload(foto[1][0]),array_fotos))
     lista_urls = list(map(lambda data: data["secure_url"],lista_imagenes))
     return lista_urls
 
-def create_pet(data,fotos):
-    body= json.loads(data)
-    company = Company_contoller.get_company(body["company_id"])
+def create_pet(data,fotos,user):
+    body = json.loads(data)
+    ckeckdata = get_volunteer_company(user["id"], body["company_id"])
     if body['name'] is None or body['name']=="":
         return {"msg": "Bad Request: Name is not correct", "error": True, "status": 400 }
-    if body['company_id'] is None:
-        return {"msg": "Bad Request: Company is not correct", "error": True, "status": 400 }
     status_id= Status.query.filter_by(type=body["status"]).one()
     pet = Repository.create_pet(body,status_id.id)
     images = upload_fotos(fotos)
     add_images= list(map(lambda url: Repository.add_url_to_pet_gallery(url, pet.id),images))
     return pet
     
-     
 def get_all_pet():
     return Repository.get_all_pet()
 
