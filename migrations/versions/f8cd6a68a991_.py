3f1db81b273c7ccd844f3d17aaa854dc8f97b552
Dihero86 <diegohrodriguez86@gmail.com>
2023-05-14 07:22:08 +0000

    feat: text dog picture

--- migrations/versions/f8cd6a68a991_.py
+++ migrations/versions/f8cd6a68a991_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: de7ea61cef28
+Revision ID: f8cd6a68a991
 Revises: 
-Create Date: 2023-05-10 09:22:25.495092
+Create Date: 2023-05-12 08:14:34.857532
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = 'de7ea61cef28'
+revision = 'f8cd6a68a991'
 down_revision = None
 branch_labels = None
 depends_on = None
@@ -62,6 +62,7 @@ def upgrade():
     )
     op.create_table('pet',
     sa.Column('id', sa.Integer(), nullable=False),
+    sa.Column('type', sa.String(length=100), nullable=False),
     sa.Column('name', sa.String(length=100), nullable=False),
     sa.Column('age', sa.Integer(), nullable=True),
     sa.Column('breed', sa.String(length=100), nullable=True),
--- src/front/js/pages/home.jsx
+++ src/front/js/pages/home.jsx
@@ -1,16 +1,24 @@
 import React, { useContext } from "react";
 import { Context } from "../store/appContext";
 import "../../styles/home.css";
-import dogImage from "../../img/dog.png";
+import dogImage from "../../img/dog.jpg";
 
 export const Home = () => {
   const { store, actions } = useContext(Context);
 
   return (
-    <div className="text-center">
-      <div className="imagen">
-        <img src={dogImage} className="img-fluid" />
+    <div className="image-container">
+      <img src={dogImage} id="portada" />
+      <div className="overlay">
+        <div className="texto">
+          <h2 className="card-title" id="title">Bienvenidos a PetsBook!</h2>
+          <p className="card-text" id="description">
+            Este es el primer sitio web donde conviven todas las protectoras de España.
+            Si quieres unirte a la familia PetsBook regístrate como usuario o
+            protectora/asociación y empieza a colaborar!</p>
+        </div>
       </div>
     </div>
+
   );
 };
