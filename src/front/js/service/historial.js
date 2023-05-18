6bfab00986eb0501e6afd8ae5a4a50d3112a93db
cpozo94 <cpg.948@gmail.com>
2023-05-18 18:00:58 +0000

    updating front_historial

--- src/front/js/service/historial.js
+++ src/front/js/service/historial.js
@@ -14,8 +14,37 @@ export const getHistorial = async (petId) => {
         Authorization: `Bearer ${token}`,
       },
     });
-    return await res.json();
+    if (res.ok) {
+      return await res.json();
+    } else {
+      console.log("Error getting pet history:", res.status);
+      return null;
+    }
   } catch (err) {
-    console.log("Error getting pet history", err);
+    console.log("Error getting pet history:", err);
+    return null;
+  }
+};
+
+export const createHistorialEntry = async (petId, entryData) => {
+  try {
+    const token = localStorage.getItem("token");
+    const res = await fetch(`${URL}/api/history/${petId}`, {
+      method: "POST",
+      headers: {
+        ...HEADERS,
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify(entryData),
+    });
+    if (res.ok) {
+      return await res.json();
+    } else {
+      console.log("Error creating historial entry:", res.status);
+      return null;
+    }
+  } catch (err) {
+    console.log("Error creating historial entry:", err);
+    return null;
   }
 };
