bb14aca7569661463240783c8754df165d9bf305
Sara Enríquez <sara_enriquezdesalamanca@hotmail.com>
2023-05-23 17:43:54 +0000

    feat all adoption processes of a company

--- src/front/js/pages/allAdoptionProcesses.jsx
+++ src/front/js/pages/allAdoptionProcesses.jsx
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from "react";
 import "../../styles/allAdoptionProcesses.css";
 import { getAllAdoptionProcesses } from "../service/adoption_process.js";
 import { useParams } from "react-router-dom";
-import { Petcard } from "../component/petcard.jsx";
 
 export const AllAdoptionProcesses = () => {
   const [adoption_processes, setAdoptionProcesses] = useState([]);
@@ -152,57 +151,3 @@ export const AllAdoptionProcesses = () => {
     </div>
   );
 };
-
-{
-  /* <ul className="adoption-process-list">
-{filter.length == 0
-  ? adoption_processes.map((adoption_process) => {
-      
-    })
-  : filter.map((adoption_process) => {
-      return (
-        <li
-          key={adoption_process.id}
-          className="adoption-process-item"
-        >
-          <div className=" adoption-process-card my-2">
-            <img
-              src={
-                adoption_process.user.avatar
-                  ? adoption_process.user.avatar
-                  : "https://res.cloudinary.com/djzijohkt/image/upload/v1683051273/icono_aq4qpy.webp"
-              }
-              className="user-avatar"
-            />
-            <img
-              src={
-                adoption_process.pet.pet_Gallery[0]
-                  ? adoption_process.pet.pet_Gallery[0]
-                  : "https://res.cloudinary.com/djzijohkt/image/upload/v1683051273/icono_aq4qpy.webp"
-              }
-              className="pet-photo"
-            />
-            <div className="card-body align-bottom adoption-process-info">
-              <p className="card-title user-name">
-                {adoption_process.user.name}{" "}
-                {adoption_process.user.last_name}
-              </p>
-              <p className="card-text m-0 user-email">
-                {adoption_process.user.email}
-              </p>
-              <p className="card-text m-0 pet-name">
-                {adoption_process.pet.name}
-              </p>
-              <p className="card-text m-0 adoption-process-description">
-                {adoption_process.description}
-              </p>
-              <p className="card-text m-0 adoption-process-status">
-                {adoption_process.status}
-              </p>
-            </div>
-          </div>
-        </li>
-      );
-    })}
-</ul> */
-}
