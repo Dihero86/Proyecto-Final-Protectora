8782e1c626ca7dc976e821e11afc13ae9c461067
Dihero86 <diegohrodriguez86@gmail.com>
2023-05-24 15:52:47 +0000

    feat: route send email

--- src/api/domain/volunteers/route.py
+++ src/api/domain/volunteers/route.py
@@ -3,7 +3,7 @@ from flask_jwt_extended import jwt_required, get_jwt
 from api.models.index import User, User_rol, CompanyVolunteers, Company
 from api.utils import generate_sitemap, APIException
 import api.domain.volunteers.controller as Controller
-
+import smtplib
 
 api = Blueprint('/api/volunteers', __name__)
 
@@ -11,3 +11,14 @@ api = Blueprint('/api/volunteers', __name__)
 def get_volunteers_of_company(company_id):
     company_volunteers= Controller.get_volunteers_of_company(company_id)
     return company_volunteers
+
+@api.route('/invite',methods=['POST'])
+def sendemail():
+    conexion = smtplib.SMTP(host='smtp.gmail.com',port=587)
+    conexion.ehlo()
+    conexion.starttls()
+    conexion.login(user="petsbookproject@gmail.com", password="")
+    mensaje="Subject:prueba\nEsto es una prueba"
+    conexion.sendmail(from_addr="petsbookproject@gmail.com", to_addrs="petsbookproject@gmail.com", msg=mensaje)
+    conexion.quit()
+    return jsonify('sent')
